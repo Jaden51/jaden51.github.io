b540121dda036444e5a6939f038448155c75761f
Jaden51 <jadenhums51@gmail.com>
2020-07-05 12:47:29 -0400

    Finished displaying quotes for portfolio

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class Quotes extends Component {
-    state = { quote: {} };
+  state = { quote: {} };
 
-    // best practice to place asyncrhous code and fetches in componentDidMount (todo)
-    componentDidMount() {
-        fetch('')
-        .then(response => response.json)
-        .then(json => console.log('json', json));
-    }
+  componentDidMount() {
+    const url = 'https://api.quotable.io/random';
+    fetch(url)
+      .then(response => response.json())
+      .then(json => this.setState({ quote: json }))
+      .catch(error => alert(error.message));
+  }
 
-    render() {
-        return (
-            <div>
-                <h2>Highlighted Quote</h2>
-            </div>
-        )
-    }
+  render() {
+    const { content, author } = this.state.quote;
+
+    return (
+      <StyledQuotes>
+        <div>
+          <h2>Highlighted Quote</h2>
+          <p className='quote'>{content} - <em>{author}</em></p>
+        </div>
+      </StyledQuotes>
+    )
+  }
 }
 
 export default Quotes;
+
+const StyledQuotes = styled.div`
+  .quote {
+    margin: 20px;
+  }
+`;
