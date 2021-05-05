3b081566927d6b6da4fa9bc0feff1c31fdbf8022
Jaden51 <jadenhums51@gmail.com>
2021-05-04 21:22:28 -0400

    Updates

---
+++ precache-manifest.bf2cc539a49e89d02e4e17fd388bd772.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7178764f3b7e93a2eb9f2cd3ef1dc85a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4de8778064d985d98e95",
+    "url": "/portfolio/static/js/2.24d1a892.chunk.js"
+  },
+  {
+    "revision": "727196b53be8768c78b2ea60f81f0ea1",
+    "url": "/portfolio/static/js/2.24d1a892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24418a41badfd5f62cc5",
+    "url": "/portfolio/static/js/main.f3b35bdc.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
